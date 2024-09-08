fd64a7fc0c57e21b61d7144b13bb3f67d77aceeb
Erivelton_Ribeiro_Pinto <erivelton@harpiaconsultoria.com>
2024-09-08 13:45:38 -0300

    First commit.

---
+++ src/main/java/br/com/invictus/InvictusApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.invictus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvictusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvictusApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/invictus/InvictusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.invictus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvictusApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
