1918f0ea624c1a48233ff582c9a3e2c34efac62a
Erivelton_Ribeiro_Pinto <erivelton@harpiaconsultoria.com>
2024-09-08 16:45:31 -0300

    Subindo uma nova versão.

---
+++ src/main/java/br/com/invictus/model/PersonModel.java
@@ -0,0 +1,14 @@
+package br.com.invictus.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "person")
+public class PersonModel implements Serializable {
+
+
+}
---
+++ src/test/java/br/com/invictus/InvictusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.invictus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvictusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
