33fea3c057ef101a174e1f76a712ab9dbb9880d7
erivelton <erivelton84ribeiro@gmail.com>
2025-04-07 20:24:30 -0300

    Criando Controller par listar Degree.

--- src/main/java/br/com/invictus/controller/DegreeController.java
+++ src/main/java/br/com/invictus/controller/DegreeController.java
@@ -1,2 +1,27 @@
-package br.com.invictus.controller;public class DegreeController {
+package br.com.invictus.controller;
+
+import br.com.invictus.enums.BeltENUM;
+import br.com.invictus.enums.DegreeENUM;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Tag(name = "Endopint de Listagem of belts.")
+@RestController
+@RequestMapping("/api/invictus/degree/v1")
+public class DegreeController {
+
+    @GetMapping
+    public List<DegreeController.DegreeDTO> getBelts() {
+        return Arrays.stream(DegreeENUM.values())
+                .map(degree -> new DegreeController.DegreeDTO(degree.getId(), degree.getDescription()))
+                .collect(Collectors.toList());
+    }
+
+    private record DegreeDTO(Integer id, String description) {}
 }
