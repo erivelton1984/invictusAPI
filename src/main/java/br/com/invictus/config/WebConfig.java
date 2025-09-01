9bf83f25edcf907c8e01619f453138a308e5451b
Erivelton Ribeiro <erivelton84ribeiro@gmail.com>
2025-09-01 10:34:08 -0300

    Update WebConfig.java

--- src/main/java/br/com/invictus/config/WebConfig.java
+++ src/main/java/br/com/invictus/config/WebConfig.java
@@ -14,7 +14,6 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.List;
 
